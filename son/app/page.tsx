424112f67489b35755b945464a01d9616faa2050
Chutipon455 <Sonchutiphon@gmail.com>
2026-06-15 10:11:07 +0700

    first commit

---
+++ son/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <ul>
+        <li>welcome to my konson studio</li>
+      </ul>
+    </div>
+  );
+}
