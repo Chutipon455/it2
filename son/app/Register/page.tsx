19f9e4f2e7bc2e84d54b561c74a71627703ea840
Chutipon455 <Sonchutiphon@gmail.com>
2026-06-29 10:24:56 +0700

    update

--- son/app/Register/page.tsx
+++ son/app/Register/page.tsx
@@ -3,10 +3,8 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-
-export default function register() {
-
-     const router = useRouter();
+export default function RegisterPage() {
+  const router = useRouter();
 
   const [form, setForm] = useState({
     name: "",
@@ -29,23 +27,30 @@ export default function register() {
     }
   }
 
-    return (
-        <div className="auth-page">
-            <form className="auth-card" onSubmit={handleSubmit}>
-                <h1>สมัครสมาชิก</h1>
-                <input placeholder="ชื่อ นามสกุล"
-                onChange={(e) => setForm({ ...form, name: e.target.value })}
-                 />
-                <input placeholder="Email" 
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
-                />
-                <input placeholder="Password"
-                onChange={(e) => setForm({ ...form, password: e.target.value })}
-                 />
-                <button> Regirter </button>
-            </form>
-        </div>
+  return (
+    <div className="auth-page">
+      <form className="auth-card" onSubmit={handleSubmit}>
+        <h1>Register</h1>
 
+        <input
+          placeholder="Name"
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
 
-    );
+        <input
+          placeholder="Email"
+          type="email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+
+        <input
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+
+        <button>Register</button>
+      </form>
+    </div>
+  );
 }
--- son/app/login/page.tsx
+++ son/app/login/page.tsx
@@ -1,15 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function register() {
+    const router = useRouter();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify(form),
+    });
+
+    if (res.ok) {
+      router.push("/dashboard");
+      router.refresh();
+    } else {
+      alert("Login failed");
+    }
+  }
     return (
         <div className="auth-page">
-            <form className="auth-card">
+            <form className="auth-card" onSubmit={handleSubmit} >
                 <h1>Login</h1>
-                <input placeholder="Email" />
-                <input placeholder="Password" />
-
-                <button> Regirter </button>
+                <input placeholder="Email"
+          type="email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+                <input  placeholder="Password"
+          type="password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+                <button> Login </button>
             </form>
         </div>
-
-
     );
 }
